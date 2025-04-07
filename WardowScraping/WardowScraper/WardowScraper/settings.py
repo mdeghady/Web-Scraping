36107a5770795dc79c67e1359fd2d13e85792c25
Abdalla Darwesh <a.darwesh@tay.local>
2025-04-07 16:15:02 +0200

    modify the settings to not obey the robotxt

--- WardowScraping/WardowScraper/WardowScraper/settings.py
+++ WardowScraping/WardowScraper/WardowScraper/settings.py
@@ -17,7 +17,7 @@ NEWSPIDER_MODULE = "WardowScraper.spiders"
 #USER_AGENT = "WardowScraper (+http://www.yourdomain.com)"
 
 # Obey robots.txt rules
-ROBOTSTXT_OBEY = True
+ROBOTSTXT_OBEY = False
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
 #CONCURRENT_REQUESTS = 32
